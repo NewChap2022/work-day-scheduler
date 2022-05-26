b22e19df65d3de7ebcb37f0e787bd2baea490b28
NewChap2022 <irisyangshu@gmail.com>
2022-05-26 10:23:06 -0400

    fix bug of saveTasks

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,4 @@
 var tasks = [];
-var currentDay = moment().format("dddd MMM Do YYYY");
-$("#currentDay").text(currentDay);
 
 var timeChecking = function (taskEl) {
     var time = moment($(taskEl).find(".hour").text().trim(), "hh A");
@@ -15,16 +13,33 @@ var timeChecking = function (taskEl) {
     };
 };
 
+var loadTasks = function() {
+    var pastTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (pastTasks === null) {
+        return false;
+    } else{}
+    // console.log(moment(pastTasks.date).isBefore(moment()));
+    // tasks = pastTasks.filter(task => moment(task.date).isBefore(moment()))
+};
 
-$(".time-block").each(function (i, obj) {
-    timeChecking(obj);
-});
+var saveTask = function (task) {
+    var savedTask = {
+        description: task.text(),
+        date: moment().format("LL"),
+        time: $(task.parents()[1]).find(".hour").text(),
+    };
+    var i = tasks.findIndex(element => element = savedTask.time);
 
-var blockAudit = setInterval(function () {
-    $(".time-block").each(function (i, obj) {
-        timeChecking(obj);
-    })
-}, (1000 * 60) * 60);
+    if (i === -1) {
+        tasks.push(savedTask);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    } else {
+        tasks[i].description = task.text();
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    };
+};
+
+$("#currentDay").text(moment().format("dddd MMM Do YYYY"));
 
 $(".task-container").on("click", function () {
     var text = $(this).find(".description")
@@ -36,25 +51,6 @@ $(".task-container").on("click", function () {
 
     $(this).find(".description").replaceWith(textInput);
 });
-
-// $(".container").on("blur", "textarea", function() {
-//     var text = $(this).parent().find(".description").text()
-//     var taskP = $("<p>")
-//     .addClass("description")
-//     .text(text);
-
-//     $(this).replaceWith(taskP);
-// });
-saveTask = function (task) {
-    var savedTask = {
-        description: task.text(),
-        date: moment().format("LL"),
-        time: $(task.parents()[1]).find(".hour").text(),
-    };
-    console.log(savedTask);
-    tasks.push(savedTask);
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-};
 
 $(".saveBtn").click(function () {
     if ($(this).parent().find("textarea").length) {
@@ -71,4 +67,21 @@ $(".saveBtn").click(function () {
     }
 });
 
+$(".time-block").each(function (i, obj) {
+    timeChecking(obj);
+});
 
+var blockAudit = setInterval(function () {
+    $(".time-block").each(function (i, obj) {
+        timeChecking(obj);
+    })
+}, (1000 * 60) * 60);
+
+// $(".container").on("blur", "textarea", function() {
+//     var text = $(this).parent().find(".description").text()
+//     var taskP = $("<p>")
+//     .addClass("description")
+//     .text(text);
+
+//     $(this).replaceWith(taskP);
+// });
